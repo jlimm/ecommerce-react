bc6eb0bcac93a5e71ba7a2ef5b785ff992bffe3d
jlimm <limjaeminn@gmail.com>
2020-12-16 15:14:40 +0900

    withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/hompage/homepage';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/hompage/homepage";
+
+const HatsPage = () => (
+  <div>
+    <h1>hatspage</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
