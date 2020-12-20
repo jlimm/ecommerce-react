8f5914cde8e2027787ce5ed37916ae85591b855d
jlimm <limjaeminn@gmail.com>
2020-12-21 00:37:11 +0900

    added login

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -6,37 +6,60 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-
   const handleSubmit = (event) => {
-      event.preventDefault();
-  }
+    event.preventDefault();
+  };
 
   const handleChange = (event) => {
-      const { target: {name, value}} = event;
-      if(name==="email"){
-          setEmail(value);
-      } else if (name ==="password") {
-          setPassword(value);
-      }
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
   };
 
   return (
-   
-    <div className="sign-in">
-      <h2>I already have an account</h2>
-      <span>Sign in with your email and password</span>
-
-      <form onSubmit={handleSubmit}>
-        <input name="email" type="email" value={email} onChange={handleChange} required />
-        <label>Email</label>
-        <input name="password" type="password" value={password} onChange={handleChange} required />
-        <label>Password</label>
-
+    <div className="container login">
+      <form onSubmit={handleSubmit} className="form form-login">
+        <header className="form-header">
+          <h1 className="heading title-login">Login</h1>
+          <p className="info-login">Please enter your e-mail and password:</p>
+        </header>
+        <div className="form-item">
+          <input
+            className="form-input"
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+          <label className="form-label">Email</label>
+        </div>
+        <div className="form-item">
+          <input
+            className="form-input"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+          <label className="form-label">Password</label>
+        </div>
         <input type="submit" value="Submit Form" />
+        <div className="login-center">
+            <span className="center-text">Don't have an account? </span>
+            <a href="/account/register" className="register-link">Create one</a>
+        </div>
       </form>
     </div>
-
-  )
+  );
 };
 
 export default Login;
